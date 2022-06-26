b2431ff1386e81b4493993935c0a7c3fb230216a
Maciej Olejnik <24421260+maciekole@users.noreply.github.com>
2022-06-26 22:23:15 +0200

    use get_absolute_url on list model to dry urls in views

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -7,7 +7,6 @@ class NewVisitorTest(FunctionalTest):
     def test_can_start_a_list_for_one_users(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        print('self.live_server_url: ', self.live_server_url)
         self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -37,3 +37,7 @@ class ListAndItemModelsTest(TestCase):
         with self.assertRaises(ValidationErr):
             item.save()
             item.full_clean()
+    
+    def test_get_absolute_url(self):
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
